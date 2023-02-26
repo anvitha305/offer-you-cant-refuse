38ba7a7ea0e6192a6d2f15f824b532c2b4d90422
Anvitha <anvitha2k20@gmail.com>
2023-02-25 21:27:06 -0500

    adding options to the popup

--- content_scripts/OYCR.js
+++ content_scripts/OYCR.js
@@ -7,19 +7,65 @@
         return;
     }
   
- // find existing ads in the page
- function adblock(){
-    adwords = ["ad", "sponsored"]
-    classes = [].concat(...[...document.querySelectorAll('*')].map(elt => [...elt.classList]));
-    classes = classes.filter(s=>adwords.some(el=>s.toLowerCase().split("-").includes(el)))
-    classes.forEach(element => {
-        document.getElementsByClassName(element).forEach(e=>e.style.border = "5px solid red");
-    });
-}
+    function addMovies(msg){
+        adblock();
+        let choices =[];
+        if (msg==="random"){
+            choices.push(...["god", "gotti", "morb"])
+        }
+        else if (msg==="morb"){
+            choices.push("morb")
+        }
+        else if (msg==="godfa"){
+            choices.push("god")
+        }
+        else if (msg==="gotti"){
+            choices.push("gotti")
+        }
+        oycr = Array.from(document.getElementsByClassName("oycrstyling"));
+        oycr.forEach(e=>{
+            let image = document.createElement("img");
+            moviename = choices[Math.floor(Math.random() * choice.length)];
+            movienum = Math.floor(Math.random() * (5)).toString();
+            movieURL="https://raw.githubusercontent.com/anvitha305/offer-you-cant-refuse/main/morb/"+moviename+movienum+".png";
+            image.setAttribute("src", movieURL);
+            image.style="width:100%; object-fit: fill;"
+            image.className="movieimage";
+            e.appendChild(image)
+            }
+        );
+    }
+// find existing ads in the page
+    function adblock(){
+        adwords = ["ad", "adContainer","ads","rail", "sponsored"]
+        classes = [].concat(...[...document.querySelectorAll('*')].map(elt => [...elt.classList]));
+        classes = classes.filter(s=>adwords.some(el=>s.toLowerCase().split("-").join("_").split(".").join("_").split("_").includes(el)))
+        classes.forEach(element => {
+            Array.from(document.getElementsByClassName(element)).forEach(e=>{
+                e.style.border = "5px solid red";
+                e.classList.add("oycrstyling");
+            });
+
+        });
+        reset()
+
+    }
+    function reset(){
+        let existing = document.getElementsByClassName("movieimage");
+        for (let b of existing) {
+            b.remove();
+        }
+        let e = document.querySelectorAll("oycrstyling");
+        for (let i of e){
+            e.style.border = "";
+        }
+    }
 
     browser.runtime.onMessage.addListener((message) => {
-        if (message.command === "adblock") {
-          adblock();
+        if (message.command === "addmovies") {
+          addMovies(message.msg);
+        } else if (message.command === "reset") {
+          reset();
         }
       });
 }
--- popup/popup.js
+++ popup/popup.js
@@ -1,22 +1,118 @@
 
-function listenForClicks() {
-    let b = document.getElementById("toggle");
-    b.addEventListener("click", (e=>{
-        if (b.innerText==="adblock on"){
-            b.innerText= "adblock off";
-        }
-        else {
-            b.innerText = "adblock on";
-        }
+const hidePage = `.oycrstyling > :not(.movieimage) {
+    display: none;
+  }`;
 
-    }
-    ));
+function listenForClicks(){
+    document.addEventListener("load", e=>{
+
+        browser.tabs.insertCSS({code: hidePage}).then(() => {
+            browser.tabs.sendMessage(tabs[0].id, {
+              command: "addmovies",
+            });
+          });
+    })
+    document.addEventListener("click", (e) => {
+
+        function adhide(tabs) {
+          browser.tabs.insertCSS({code: hidePage}).then(() => {
+            browser.tabs.sendMessage(tabs[0].id, {
+              command: "addmovies",
+              msg: "random"
+            });
+          });
+        }
+        function morb(tabs) {
+            browser.tabs.insertCSS({code: hidePage}).then(() => {
+              browser.tabs.sendMessage(tabs[0].id, {
+                command: "addmovies",
+                msg: "morb"
+              });
+            });
+          }
+          function gotti(tabs) {
+            browser.tabs.insertCSS({code: hidePage}).then(() => {
+              browser.tabs.sendMessage(tabs[0].id, {
+                command: "addmovies",
+                msg: "gotti"
+              });
+            });
+          }
+          function godfa(tabs) {
+            browser.tabs.insertCSS({code: hidePage}).then(() => {
+              browser.tabs.sendMessage(tabs[0].id, {
+                command: "addmovies",
+                msg: "godfa"
+              });
+            });
+          }
+    
+        function reset(tabs) {
+          browser.tabs.removeCSS({code: hidePage}).then(() => {
+            browser.tabs.sendMessage(tabs[0].id, {
+              command: "reset"
+            });
+          });
+        }
+    
+        /**
+         * Just log the error to the console.
+         */
+        function reportError(error) {
+          console.error(`Could not moviefy: ${error}`);
+        }
+    
+        /**
+         * Get the active tab,
+         * then call "beastify()" or "reset()" as appropriate.
+         */
+        if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
+          // Ignore when click is not on a button within <div id="popup-content">.
+          return;
+        } 
+        if (e.target.type === "reset") {
+          browser.tabs.query({active: true, currentWindow: true})
+            .then(reset)
+            .catch(reportError);
+        } else if (e.target.type ==="morbi"){
+            browser.tabs.query({active: true, currentWindow: true})
+            .then(morb)
+            .catch(reportError);
+        }
+        else if (e.target.type ==="godfa"){
+            browser.tabs.query({active: true, currentWindow: true})
+            .then(godfa)
+            .catch(reportError);
+        }
+        else if (e.target.type ==="gotti"){
+            browser.tabs.query({active: true, currentWindow: true})
+            .then(gotti)
+            .catch(reportError);
+        }
+        else{
+          browser.tabs.query({active: true, currentWindow: true})
+            .then(adhide)
+            .catch(reportError);
+        }
+      })    
 }
-function reportExecuteScriptError(error) {
-    console.error(`Failed to execute Offer You Can't Refuse content script: ${error.message}`);
- }
 
+/**
+ * There was an error executing the script.
+ * Display the popup's error message, and hide the normal UI.
+ */
+function reportExecuteScriptError(error) {
+    document.querySelector("#popup-content").classList.add("hidden");
+    document.querySelector("#error-content").classList.remove("hidden");
+    console.error(`Failed to execute oycr content script: ${error.message}`);
+  }
   
-browser.tabs.executeScript({file: "/content_scripts/OYCR.js"})
-.then(listenForClicks)
-.catch(reportExecuteScriptError);
+  /**
+   * When the popup loads, inject a content script into the active tab,
+   * and add a click handler.
+   * If we couldn't inject the script, handle the error.
+   */
+  browser.tabs.executeScript({file: "/content_scripts/OYCR.js"})
+  .then(listenForClicks)
+  .catch(reportExecuteScriptError);
+  
